47ec57805f8a44fc4df9dd7da27f48baea9f7c03
Heiko Mattern <hwm@medicuja.com>
2016-12-18 20:08:22 +0100

    + better navbar + mandelbrot.js

--- web/js/p5/mandelbrot/mandelbrot.js
+++ web/js/p5/mandelbrot/mandelbrot.js
@@ -1,45 +1,63 @@
-var sizeX = 500;
-var sizeY = 500;
+var sz = 500;
 
-var iteration = 30;
+var iteration = 10;
 var constA    = 0;
 var constB    = 0;
-var threshold = 1000;
-var scl       = 0.01;
+var threshold = 10000;
+var scl       = 0.025;
 
 function isIn(a, b) {
-    var oldA = (a - sizeX/2)*scl;
-    var oldB = (b - sizeY/2)*scl;
+    // center a and b
+    var oldA = transScale(a);
+    var oldB = transScale(b);
 
     var newA = 0;
     var newB = 0;
+
+    var amount = 0;
+
     for (var i = 0; i < this.iteration; i++) {
-        newA = oldA*oldA - oldB*oldB + this.constA;
-        newB = 2*oldA*oldB + this.constB;
+
+        newA = oldA*oldA - oldB*oldB + transScale(this.constA);
+        newB = 2*oldA*oldB           + transScale(this.constB);
+
         oldA = newA;
         oldB = newB;
+
+        amount = Math.abs(newA + newB);
+
+        if (amount > threshold) {
+            console.log(amount);
+            return false;
+        }
     }
-    var amount = Math.abs(newA) + Math.abs(newB);
-    return amount < threshold;
+
+    return true;
+}
+
+function transScale(val) {
+    return (val - sz/2)*scl;
 }
 
 function setup() {
+    xnoLoop();
     frameRate(1);
-    createCanvas(sizeX, sizeY);
+    createCanvas(sz, sz);
 }
 
 function draw() {
+    console.log('from:' + transScale(0), 'to:' + transScale(sz));
     background(51);
     var counter = 0;
-    for (var i = 0; i < sizeX; i++) {
-        for(var j = 0; j < sizeY; j++) {
+    for (var i = 0; i < sz; i++) {
+        for(var j = 0; j < sz; j++) {
             if (isIn(i,j)) {
                 counter++;
-                stroke('white');
-                fill('white');
+                stroke('green');
+                fill('green');
                 point(i,j);
+                console.log('in');
             }
         }
     }
-    console.log(counter);
 }
