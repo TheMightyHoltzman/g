2569c2ce22d752acfc20c51f82fbb55bdabaed15
Heiko <matternheiko@gmail.com>
2017-04-25 23:23:04 +0200

    Fully functioning tetris.

--- web/js/p5/tetris/tetris.js
+++ web/js/p5/tetris/tetris.js
@@ -21,7 +21,7 @@ var shapes = [
 // render-variables
 var scl        = 30;
 var gridWidth  = 10;
-var gridHeight = 10;
+var gridHeight = 20;
 
 // game variables
 var boulder    = null;
@@ -55,13 +55,12 @@ function draw() {
   background(51);
 
   board.cancelRows();
+  boulder.drop();
+  boulder.render();
 
   if(hasCollision()) {
     boulder.setup(difficulty);
   }
-
-  boulder.move();
-  boulder.render();
 
   board.render();
 }
@@ -122,7 +121,7 @@ function Board() {
         for (var row = 0; row < gridWidth; row++) {
           if (rows[row]) {
             console.log('sliced it');
-            grid[col].slice(row, 1);
+            grid[col].splice(row, 1);
             grid[col].unshift(false);
           }
         }
@@ -139,7 +138,7 @@ function Boulder() {
   this.dir      = null;
 
   this.setup = function(difficulty) {
-      var index  = Math.floor(random(0, shapes.length - 1));
+      var index  = Math.floor(random(0, shapes.length));
 
       this.x        = toGrid(gridWidth*scl/2);
       this.y        = 0;
@@ -151,16 +150,20 @@ function Boulder() {
       this.rotation = 0;
   };
 
-  this.move = function() {
+  this.move = function(dir) {
     var collides = false;
     this.iterator(function(x, y, dir) {
       if (x + dir < 0 || x + dir >= gridWidth || grid[x + dir][y]) {
         collides = true;
+        console.log('collides');
       }
     });
     if (!collides) {
         this.x += this.dir;
     }
+  };
+
+  this.drop = function() {
     if (this.fall === 0) {
       this.y += 1;
       this.fall = difficulty;
@@ -171,7 +174,19 @@ function Boulder() {
   };
 
   this.rotate  = function(dir) {
-      this.rotation = (this.rotation+1)%this.shape.length;
+    this.rotation = (this.rotation+1)%this.shape.length;
+    var collides = false;
+
+    this.iterator(function(x, y, dir) {
+      if (x < 0 || x >= gridWidth || grid[x][y]) {
+        collides = true;
+        console.log('collides');
+      }
+    });
+
+    if (collides) {
+        this.rotation = (this.rotation-1)%this.shape.length;
+    }
   };
 
   this.iterator = function(onEach) {
@@ -189,11 +204,16 @@ function Boulder() {
   };
 }
 
+
 function keyPressed() {
   if(keyCode === RIGHT_ARROW) {
       boulder.dir = 1;
+      boulder.move();
+      console.log('Moved 1');
   } else if(keyCode === LEFT_ARROW) {
-      boulder.dir = -1;
+      boulder.dir = -1,
+      boulder.move();
+      console.log('Moved -1');
   } else if(keyCode === UP_ARROW) {
       boulder.rotate();
   }
@@ -204,7 +224,5 @@ function keyReleased() {
       boulder.dir = 0;
   } else if(keyCode === LEFT_ARROW) {
       boulder.dir = 0;
-  } else if(keyCode === LEFT_ARROW) {
-      boulder.dir = -1;
   }
 }
